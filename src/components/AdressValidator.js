4411a14b96595cdb595c216fca0b1f7909455163
B3SUS <IB3SUSI@gmail.com>
2024-06-22 02:42:35 +0300

    recent working

--- src/components/AdressValidator.js
+++ src/components/AdressValidator.js
@@ -10,7 +10,7 @@ export const AdressValidator  = ({isValid, address, setAddress, activeCoin2, sel
     return (
         <div id={'field'} className={`relative rounded-[.5rem] mt-[.2em] pointer-events-auto text-center text-[12px] text-white`}>
             <input
-                className={`rounded-[.5rem] h-[53px] pr-[6.8em] box-border bg-black/[.5] border border-solid border-transparent text-[1.1em] leading-[1] py-[1.05em] pl-[1em] w-full resize-none block overflow-hidden pointer-events-auto text-left focus:outline-none font-[Mont]`}
+                className={`rounded-[.5rem] h-[53px] pr-[6.8em] box-border bg-black/[.5] border border-solid border-transparent text-[1.1em] leading-[1] py-[1.05em] pl-[1em] w-full resize-none block overflow-hidden pointer-events-auto text-left focus:outline-none font-[MontMed]`}
                 required={true} autoComplete={`off`} placeholder={rateCheck} value={address}
                 onChange={(e) => setAddress(e.target.value)}
             ></input>
